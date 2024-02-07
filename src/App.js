a82d7324a4372d13d48c1194a3e2713a48a3f138
firas hamila <firas.hm.1996@gmail.com>
2024-02-07 15:48:22 +0100

    qq

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
+  const expensesData = [
+    {
+      id: 1,
+      price: 7999,
+      title: "NEw TV",
+      date: new Date(2024, 2, 15),
+    },
+    {
+      id: 2,
+      price: 3000,
+      title: "New Phone",
+      date: new Date(2023, 8, 1),
+    },
+    {
+      id: 3,
+      price: 5000,
+      title: "Education",
+      date: new Date(2023, 10, 15),
+    },
+    {
+      id: 4,
+      price: 2500,
+      title: "Voyage",
+      date: new Date(2025, 7, 30),
+    },
+  ];
   return (
     <div>
-      <ExpenseItem />
+      {expensesData.map((x) => {
+        return <ExpenseItem title={x.title} price={x.price} date={x.date} />;
+      })}
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,12 @@
-import React from "react";
+import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ title, price, date }) => {
   return (
     <div className="expense-item">
       <div>date</div>
       <div className="expense-item__description">
-        <h2>title</h2>
-        <div className="expense-item__price">price</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{price}</div>
       </div>
     </div>
   );
