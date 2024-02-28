2e3f456f1de58c53825750f183f23ddf1744eac0
firas hamila <firas.hm.1996@gmail.com>
2024-02-28 14:02:20 +0100

    new

--- src/Components/ExpensesFilter.js
+++ src/Components/ExpensesFilter.js
@@ -3,7 +3,7 @@ import "./ExpensesFilter.css";
 const ExpensesFilter = ({ years }) => {
   return (
     <div className="expenses-filter expenses-filter__control">
-      <label>Filter by year</label>
+      <label>Filter By Year</label>
       <select>
         {years.map((item) => {
           return <option value={item}>{item}</option>;
